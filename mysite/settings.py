a3411e5a3516def035d5f133f5ce3153933667a9
mohammdseyfipc <mohammadseyfipc@gmail.com>
2024-02-27 23:00:29 +0330

    change database sqlite 3 to mysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,10 +103,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Travel',
+        'USER': 'admin',
+        'PASSWORD': 'hosseinseyfi80',
+        'HOST': 'localhost',  # Or your MySQL server IP address
+        'PORT': '3306',       # MySQL default port
+    },
 }
 
 
